d9399ae8c69075c33f94fede6f8b2a3bae6ba399
shoebdindore-source <shoebdindore@gmail.com>
2026-05-10 22:43:46 +0530

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,18 @@
+#Question1)
+arr=list(map(int,input().split()))
+
+key=int(input())
+found=False
+for i in range(len(arr)):
+	if arr[i]==key:
+		print(i)
+		found=True
+		break
+if not found:
+	print("Not found")
+
+
+#Question2)
+heights=list(map(int,input().split()))
+tallest=max(heights)
+print(tallest)
